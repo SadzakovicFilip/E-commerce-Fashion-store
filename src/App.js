170b1c145c7fce2e21cf49e30a4d98f58f65b557
SadzakovicFilip <filipsadzakovic.dev@gmail.com>
2023-09-21 19:52:20 +0200

    Added scss and bgImages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
+import "./categories.styles.scss";
+
 const App = () => {
   const categories = [
     {
-      title: "Hats",
+      id: 1,
+      title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
     },
     {
-      title: "Jackets",
+      id: 2,
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
     },
     {
-      title: "Sneekers",
+      id: 3,
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
     },
     {
-      title: "Women",
+      id: 4,
+      title: "womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
     },
     {
-      title: "Men",
+      id: 5,
+      title: "mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
     },
   ];
+
   return (
     <div className="categories-container">
       {categories.map((item, key) => (
         <div className="category-container" key={key}>
-          <div className="background-image" />
+          <div
+            className="background-image"
+            style={{ backgroundImage: `url(${item.imageUrl})` }}
+          />
           <div className="category-body-container">
             <h2>{item.title}</h2>
             <p>Shop Now</p>

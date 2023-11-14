fd368e9c66caab55b67bab6e50295bae5a0152b1
SadzakovicFilip <filipsadzakovic.dev@gmail.com>
2023-11-14 17:20:41 +0100

    Implemented STRIPE completly, only thing left to do is create a Stripe account in order to get publishable key and secret key from an account

--- src/components/payment-form/payment-form.component.js
+++ src/components/payment-form/payment-form.component.js
@@ -1,24 +1,61 @@
+import { useState } from "react";
+import { useSelector } from "react-redux/es/hooks/useSelector";
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+
+import { selectCartTotal } from "../../store/cart/cart.selector";
+import { selectCurrentUser } from "../../store/user/user.selector";
 
 import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
 import { PaymentFormContainer, FormContainer } from "./payment-form.styles";
-import { async } from "@firebase/util";
 const PaymentForm = () => {
   const stripe = useStripe();
   const elements = useElements();
+  const amount = useSelector(selectCartTotal);
+  const currentUser = useSelector(selectCurrentUser);
+  const [isProcessingPayment, setIsProcessingPayment] = useState(false);
 
   const paymentHandler = async (e) => {
     e.preventDefault();
     if (!stripe || !elements) {
       return;
     }
+    setIsProcessingPayment(true);
+    const response = await fetch(`/.netlify/functions/create-payment-intent`, {
+      method: "post",
+      headers: {
+        "Content-Type": `aplication/json`,
+      },
+      body: JSON.stringify({ amount: amount * 100 }),
+    }).then((res) => res.json);
+    console.log(response);
+    const {
+      paymentIntent: { client_secret },
+    } = response;
+    const paymentResult = await stripe.confirmCardPayment(client_secret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: currentUser ? currentUser.displayName : `Guest`,
+        },
+      },
+    });
+    setIsProcessingPayment(false);
+    if (paymentResult.error) {
+      alert(paymentResult.error);
+    } else {
+      if (paymentResult.paymentIntent.status === `succeeded`) {
+        alert(`payment Succesful`);
+      }
+    }
   };
+
   return (
     <PaymentFormContainer>
-      <FormContainer>
+      <FormContainer onSubmit={paymentHandler}>
         <h2>Credit Card Payment : </h2>
         <CardElement />
         <Button
+          isLoading={isProcessingPayment}
           style={{ marginTop: "10px" }}
           buttonType={BUTTON_TYPE_CLASSES.inverted}
         >
